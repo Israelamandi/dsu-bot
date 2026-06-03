e6934a861a526d59a2a50418c685e8e001966a33
Israel Amandi <52631626+Israelamandi@users.noreply.github.com>
2026-06-03 22:15:41 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,7 @@
+module.exports = [
+  "What did you accomplish yesterday?",
+  "What are your priorities today?",
+  "Any blockers?",
+  "Do you need support from anyone?",
+  "Which area/client/actions need attention today?"
+];
